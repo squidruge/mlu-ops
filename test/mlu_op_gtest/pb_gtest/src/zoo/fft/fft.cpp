d0dd5ea922db4535df64d07fdafcce97e02f60af
root <1525472560@qq.com>
2024-06-03 22:08:01 +0800

    [Fix](mluOpExecFFT): update common utils

--- test/mlu_op_gtest/pb_gtest/src/zoo/fft/fft.cpp
+++ test/mlu_op_gtest/pb_gtest/src/zoo/fft/fft.cpp
@@ -22,226 +22,261 @@
  *************************************************************************/
 #include "fft.h"
 #include <fftw3.h>
-namespace mluoptest {
+namespace mluoptest
+{
 
-void FftExecutor::paramCheck() {
-  GTEST_CHECK(parser_->getInputNum() == 1, "fft input number is wrong.");
-  GTEST_CHECK(parser_->getOutputNum() == 1, "fft input number is wrong.");
-}
-
-void FftExecutor::workspaceMalloc() {
-  // return;
-  auto input_tensor = tensor_desc_[0].tensor;
-  auto output_tensor = tensor_desc_[1].tensor;
-
-  auto fft_param = parser_->getProtoNode()->fft_param();
-  int rank = fft_param.rank();
-  std::vector<int> n;
-  for (int i = 0; i < rank; i++) {
-    n.push_back(fft_param.n(i));
+  void FftExecutor::paramCheck()
+  {
+    GTEST_CHECK(parser_->getInputNum() == 1, "fft input number is wrong.");
+    GTEST_CHECK(parser_->getOutputNum() == 1, "fft input number is wrong.");
   }
-  int direction = fft_param.direction();
 
-  MLUOP_CHECK(mluOpCreateFFTPlan(&fft_plan_));
-  MLUOP_CHECK(mluOpMakeFFTPlanMany(
-      handle_, fft_plan_, input_tensor, output_tensor, rank, n.data(),
-      &reservespace_size_, &workspace_size_));
+  void FftExecutor::workspaceMalloc()
+  {
+    // return;
+    auto input_tensor = tensor_desc_[0].tensor;
+    auto output_tensor = tensor_desc_[1].tensor;
 
-  VLOG(4) << "reserve space size: " << reservespace_size_;
-  VLOG(4) << "workspace size: " << workspace_size_;
+    auto fft_param = parser_->getProtoNode()->fft_param();
+    int rank = fft_param.rank();
+    std::vector<int> n;
+    for (int i = 0; i < rank; i++)
+    {
+      n.push_back(fft_param.n(i));
+    }
+    int direction = fft_param.direction();
 
-  if (reservespace_size_ > 0) {
-    GTEST_CHECK(reservespace_addr_ = mlu_runtime_.allocate(reservespace_size_));
-    workspace_.push_back(reservespace_addr_);
+    MLUOP_CHECK(mluOpCreateFFTPlan(&fft_plan_));
+    MLUOP_CHECK(mluOpMakeFFTPlanMany(
+        handle_, fft_plan_, input_tensor, output_tensor, rank, n.data(),
+        &reservespace_size_, &workspace_size_));
+
+    VLOG(4) << "reserve space size: " << reservespace_size_;
+    VLOG(4) << "workspace size: " << workspace_size_;
+
+    if (reservespace_size_ > 0)
+    {
+      GTEST_CHECK(reservespace_addr_ = mlu_runtime_.allocate(reservespace_size_));
+      workspace_.push_back(reservespace_addr_);
+    }
+    //  interface_timer_.start();
+    /* reserve space is the compiling time process before FFT execution */
+    MLUOP_CHECK(mluOpSetFFTReserveArea(handle_, fft_plan_, reservespace_addr_));
+    //  interface_timer_.stop();
+    if (workspace_size_ > 0)
+    {
+      GTEST_CHECK(workspace_addr_ = mlu_runtime_.allocate(workspace_size_));
+      workspace_.push_back(workspace_addr_);
+    }
   }
-  //  interface_timer_.start();
-  /* reserve space is the compiling time process before FFT execution */
-  MLUOP_CHECK(mluOpSetFFTReserveArea(handle_, fft_plan_, reservespace_addr_));
-  //  interface_timer_.stop();
-  if (workspace_size_ > 0) {
-    GTEST_CHECK(workspace_addr_ = mlu_runtime_.allocate(workspace_size_));
-    workspace_.push_back(workspace_addr_);
+
+  void FftExecutor::compute()
+  {
+    // return;
+    VLOG(4) << "FftExecutor compute ";
+    auto input_dev = data_vector_[0].device_ptr;
+    auto output_dev = data_vector_[1].device_ptr;
+    // auto input_tensor = tensor_desc_[0].tensor;
+    // auto output_tensor = tensor_desc_[1].tensor;
+    auto fft_param = parser_->getProtoNode()->fft_param();
+    int direction = fft_param.direction();
+    float scale_factor = fft_param.scale_factor();
+
+    VLOG(4) << "call mluOpFFT";
+
+    interface_timer_.start();
+    MLUOP_CHECK(mluOpExecFFT(handle_, fft_plan_, input_dev, scale_factor,
+                             workspace_addr_, output_dev, direction));
+    interface_timer_.stop();
   }
-}
 
-void FftExecutor::compute() {
-  // return;
-  VLOG(4) << "FftExecutor compute ";
-  auto input_dev = data_vector_[0].device_ptr;
-  auto output_dev = data_vector_[1].device_ptr;
-  // auto input_tensor = tensor_desc_[0].tensor;
-  // auto output_tensor = tensor_desc_[1].tensor;
-  auto fft_param = parser_->getProtoNode()->fft_param();
-  int direction = fft_param.direction();
-  float scale_factor = fft_param.scale_factor();
-
-  VLOG(4) << "call mluOpFFT";
-
-  interface_timer_.start();
-  MLUOP_CHECK(mluOpExecFFT(handle_, fft_plan_, input_dev, scale_factor,
-                           workspace_addr_, output_dev, direction));
-  interface_timer_.stop();
-}
-
-void FftExecutor::workspaceFree() {
-  MLUOP_CHECK(mluOpDestroyFFTPlan(fft_plan_));
-  for (auto& addr : workspace_) {
-    mlu_runtime_.deallocate(addr);
+  void FftExecutor::workspaceFree()
+  {
+    MLUOP_CHECK(mluOpDestroyFFTPlan(fft_plan_));
+    for (auto &addr : workspace_)
+    {
+      mlu_runtime_.deallocate(addr);
+    }
+    workspace_.clear();
   }
-  workspace_.clear();
-}
 
-void FftExecutor::cpuCompute() {
-  // TODO(sunhui): use fftw? librosa? OTFFT? other thrid-party library.
-  // auto batch = parser_->getInputDataCount(0);
-  auto count = parser_->getInputDataCount(0);
-  // printf("\n\n\nbatch: %ld, count: %ld\n\n\n", batch, count);
-  // cpu_fp32_output_[0][i] = (cpu_fp32_input_[0][i]);
+  void FftExecutor::cpuCompute()
+  {
+    // TODO(sunhui): use fftw? librosa? OTFFT? other thrid-party library.
+    // auto batch = parser_->getInputDataCount(0);
+    auto count = parser_->getInputDataCount(0);
+    // printf("\n\n\nbatch: %ld, count: %ld\n\n\n", batch, count);
+    // cpu_fp32_output_[0][i] = (cpu_fp32_input_[0][i]);
 
-  for (int i = 0; i < count; ++i) {
-    cpu_fp32_output_[0][i] = (cpu_fp32_input_[0][i]);
-  }
+    for (int i = 0; i < count; ++i)
+    {
+      cpu_fp32_output_[0][i] = (cpu_fp32_input_[0][i]);
+    }
 
 #define TEST_C2C1D_FP32 0
 #define TEST_C2C2D_FP32 1
 
 #if TEST_C2C1D_FP32
-  auto size = count / 32;
-  fftwf_plan fft;
+    int n[1];
+    n[0] = parser_->getProtoNode()->fft_param().n()[0];
 
-  fftwf_complex* fftw_out = ((fftwf_complex*)cpu_fp32_output_[0]);
-  fftwf_complex* fftw_in = ((fftwf_complex*)cpu_fp32_input_[0]);
+    int batch = count / n[0];
 
-  // int rank = 1;
-  // int rank = fft_param.rank();
-  int howmany = 32;
-  // fft = fftwf_plan_many_dft(rank, &size, howmany, fftw_in, inembed, istride,
-  // idist,
-  //                           fftw_out, onembed, ostride, odist,
-  //                           FFTW_FORWARD, FFTW_MEASURE);
+    auto size = count / batch;
+    fftwf_plan fft;
 
-  // fft = fftwf_plan_dft_1d(size, fftw_in, fftw_out, FFTW_FORWARD,
-  //                         FFTW_ESTIMATE);  // Setup fftw plan for fft
+    fftwf_complex *fftw_out = ((fftwf_complex *)cpu_fp32_output_[0]);
+    fftwf_complex *fftw_in = ((fftwf_complex *)cpu_fp32_input_[0]);
 
-  // fftwf_execute(fft);
+    // int rank = 1;
+    // int rank = fft_param.rank();
+    int howmany = batch;
+    // fft = fftwf_plan_many_dft(rank, &size, howmany, fftw_in, inembed, istride,
+    // idist,
+    //                           fftw_out, onembed, ostride, odist,
+    //                           FFTW_FORWARD, FFTW_MEASURE);
 
-  for (int batch_id = 0; batch_id < 32; batch_id++) {
-    fft = fftwf_plan_dft_1d(size, fftw_in + batch_id * size,
-                            fftw_out + batch_id * size, FFTW_FORWARD,
-                            FFTW_ESTIMATE);  // Setup fftw plan for fft
+    // fft = fftwf_plan_dft_1d(size, fftw_in, fftw_out, FFTW_FORWARD,
+    //                         FFTW_ESTIMATE);  // Setup fftw plan for fft
 
-    fftwf_execute(fft);
-  }
-  fftwf_destroy_plan(fft);
+    // fftwf_execute(fft);
+
+    for (int batch_id = 0; batch_id < batch; batch_id++)
+    {
+      fft = fftwf_plan_dft_1d(size, fftw_in + batch_id * size,
+                              fftw_out + batch_id * size, FFTW_FORWARD,
+                              FFTW_ESTIMATE); // Setup fftw plan for fft
+
+      fftwf_execute(fft);
+    }
+    fftwf_destroy_plan(fft);
 #endif
 
 #if TEST_C2C2D_FP32
-  fftwf_plan fft;
+    fftwf_plan fft;
 
-  fftwf_complex* fftw_out = ((fftwf_complex*)cpu_fp32_output_[0]);
-  fftwf_complex* fftw_in = ((fftwf_complex*)cpu_fp32_input_[0]);
+    fftwf_complex *fftw_out = ((fftwf_complex *)cpu_fp32_output_[0]);
+    fftwf_complex *fftw_in = ((fftwf_complex *)cpu_fp32_input_[0]);
 
-  // int rank = 1;
-  // int rank = fft_param.rank();
-  // int howmany = 32;
-  // fft = fftwf_plan_many_dft(rank, &size, howmany, fftw_in, inembed, istride,
-  // idist,
-  //                           fftw_out, onembed, ostride, odist,
-  //                           FFTW_FORWARD, FFTW_MEASURE);
+    // int rank = 1;
+    // int rank = fft_param.rank();
+    // int howmany = 32;
+    // fft = fftwf_plan_many_dft(rank, &size, howmany, fftw_in, inembed, istride,
+    // idist,
+    //                           fftw_out, onembed, ostride, odist,
+    //                           FFTW_FORWARD, FFTW_MEASURE);
 
-  // fft = fftwf_plan_dft_1d(size, fftw_in, fftw_out, FFTW_FORWARD,
-  //                         FFTW_ESTIMATE);  // Setup fftw plan for fft
+    // fft = fftwf_plan_dft_1d(size, fftw_in, fftw_out, FFTW_FORWARD,
+    //                         FFTW_ESTIMATE);  // Setup fftw plan for fft
 
-  // fftwf_execute(fft);
-  // printf("count: %ld\n", count);
-  // [batch, n0, n1]
-  int n[2];
-  n[0] = parser_->getProtoNode()->fft_param().n()[0];
-  n[1] = parser_->getProtoNode()->fft_param().n()[1];
+    // fftwf_execute(fft);
+    // printf("count: %ld\n", count);
+    // [batch, n0, n1]
+    int n[2];
+    n[0] = parser_->getProtoNode()->fft_param().n()[0];
+    n[1] = parser_->getProtoNode()->fft_param().n()[1];
 
-  fft = fftwf_plan_dft_2d(n[0], n[1], fftw_in, fftw_out, FFTW_FORWARD,
-                          FFTW_ESTIMATE);  // Setup fftw plan for fft
+    fft = fftwf_plan_dft_2d(n[0], n[1], fftw_in, fftw_out, FFTW_FORWARD,
+                            FFTW_ESTIMATE); // Setup fftw plan for fft
 
-  fftwf_execute(fft);
-  fftwf_destroy_plan(fft);
+    fftwf_execute(fft);
+    fftwf_destroy_plan(fft);
 
 #endif
-}
-
-int64_t FftExecutor::getTheoryOps() {
-  auto input_tensor = tensor_desc_[0].tensor;
-  auto fft_param = parser_->getProtoNode()->fft_param();
-  int rank = fft_param.rank();
-  int bc = 1;
-  if (input_tensor->dim != rank) {
-    bc = input_tensor->dims[0];
   }
-  int n = fft_param.n(0);
 
-  int64_t ops_each_batch;
-  // Convert LT and CT computing power. The computing power of a single LT is
-  // 4096 * 2, the computing power of a single CT is 128.
-  int cp_ratio = 4096 * 2 / 128;
-  if (n <= 4096) {
-    // fft_plan->fft_strategy = CNFFT_FUNC_MATMUL. Mainly use LT.
-    ops_each_batch = n * n * 2 / cp_ratio;
-  } else {
-    ops_each_batch = n * int(std::log(n)) * 2;
-    // fft_plan->fft_strategy = CNFFT_FUNC_COOLEY_TUKEY or CNFFT_FUNC_STOCKHAM.
-    // Half use LT and half use CT.
-    ops_each_batch = ops_each_batch * (0.5 / cp_ratio + 0.5);
-  }
-  int64_t theory_ops = bc * ops_each_batch;
-  VLOG(4) << "getTheoryOps: " << theory_ops << " ops";
-  return theory_ops;
-}
-
-int64_t FftExecutor::getTheoryIoSize() {
-  // dtype check
-  auto input_tensor = tensor_desc_[0].tensor;
-  auto output_tensor = tensor_desc_[1].tensor;
-  mluOpDataType_t input_dtype = input_tensor->dtype;
-  mluOpDataType_t output_dtype = output_tensor->dtype;
-
-  auto fft_param = parser_->getProtoNode()->fft_param();
-  int rank = fft_param.rank();
-  int bc = 1;
-  if (input_tensor->dim != rank) {
-    bc = input_tensor->dims[0];
-  }
-  int n = fft_param.n(0);
-
-  int64_t theory_ios = 0;
-  if (n <= 4096) {
-    if (input_dtype == output_dtype) {
-      theory_ios += bc * n * 4;  // matmul io
-    } else {                     // r2c or c2r
-      theory_ios += bc * n * 2;  // matmul io
+  int64_t FftExecutor::getTheoryOps()
+  {
+    auto input_tensor = tensor_desc_[0].tensor;
+    auto fft_param = parser_->getProtoNode()->fft_param();
+    int rank = fft_param.rank();
+    int bc = 1;
+    if (input_tensor->dim != rank)
+    {
+      bc = input_tensor->dims[0];
     }
-    theory_ios += n * n * 2;  // W io
-  } else {
-    if (input_dtype == output_dtype) {
-      theory_ios += bc * n * 4;  // matmul io
-      theory_ios += bc * n * 4;  // stockham or cooley_tukey io
-    } else {                     // r2c or c2r
-      theory_ios += bc * n * 2;  // matmul
-      theory_ios += bc * n * 2;  // stockham or cooley_tukey io
-    }
+    int n = fft_param.n(0);
 
-    // W io
-    int n_temp = n;
-    while (n_temp >= 128 && n_temp % 2 == 0) {
-      n_temp = n_temp / 2;
+    int64_t ops_each_batch;
+    // Convert LT and CT computing power. The computing power of a single LT is
+    // 4096 * 2, the computing power of a single CT is 128.
+    int cp_ratio = 4096 * 2 / 128;
+    if (n <= 4096)
+    {
+      // fft_plan->fft_strategy = CNFFT_FUNC_MATMUL. Mainly use LT.
+      ops_each_batch = n * n * 2 / cp_ratio;
     }
-    theory_ios += n_temp * 2;
+    else
+    {
+      ops_each_batch = n * int(std::log(n)) * 2;
+      // fft_plan->fft_strategy = CNFFT_FUNC_COOLEY_TUKEY or CNFFT_FUNC_STOCKHAM.
+      // Half use LT and half use CT.
+      ops_each_batch = ops_each_batch * (0.5 / cp_ratio + 0.5);
+    }
+    int64_t theory_ops = bc * ops_each_batch;
+    VLOG(4) << "getTheoryOps: " << theory_ops << " ops";
+    return theory_ops;
   }
-  VLOG(4) << "getTheoryIoSize: " << theory_ios << " ops";
-  return theory_ios;
-}
 
-std::set<Evaluator::Formula> FftExecutor::getCriterionsUse() const {
-  return {Evaluator::DIFF1, Evaluator::DIFF2, Evaluator::DIFF3,
-          Evaluator::DIFF4};
-}
+  int64_t FftExecutor::getTheoryIoSize()
+  {
+    // dtype check
+    auto input_tensor = tensor_desc_[0].tensor;
+    auto output_tensor = tensor_desc_[1].tensor;
+    mluOpDataType_t input_dtype = input_tensor->dtype;
+    mluOpDataType_t output_dtype = output_tensor->dtype;
 
-}  // namespace mluoptest
+    auto fft_param = parser_->getProtoNode()->fft_param();
+    int rank = fft_param.rank();
+    int bc = 1;
+    if (input_tensor->dim != rank)
+    {
+      bc = input_tensor->dims[0];
+    }
+    int n = fft_param.n(0);
+
+    int64_t theory_ios = 0;
+    if (n <= 4096)
+    {
+      if (input_dtype == output_dtype)
+      {
+        theory_ios += bc * n * 4; // matmul io
+      }
+      else
+      {                           // r2c or c2r
+        theory_ios += bc * n * 2; // matmul io
+      }
+      theory_ios += n * n * 2; // W io
+    }
+    else
+    {
+      if (input_dtype == output_dtype)
+      {
+        theory_ios += bc * n * 4; // matmul io
+        theory_ios += bc * n * 4; // stockham or cooley_tukey io
+      }
+      else
+      {                           // r2c or c2r
+        theory_ios += bc * n * 2; // matmul
+        theory_ios += bc * n * 2; // stockham or cooley_tukey io
+      }
+
+      // W io
+      int n_temp = n;
+      while (n_temp >= 128 && n_temp % 2 == 0)
+      {
+        n_temp = n_temp / 2;
+      }
+      theory_ios += n_temp * 2;
+    }
+    VLOG(4) << "getTheoryIoSize: " << theory_ios << " ops";
+    return theory_ios;
+  }
+
+  std::set<Evaluator::Formula> FftExecutor::getCriterionsUse() const
+  {
+    return {Evaluator::DIFF1, Evaluator::DIFF2, Evaluator::DIFF3,
+            Evaluator::DIFF4};
+  }
+
+} // namespace mluoptest
